b54d960e848bff5708a52a23d9a55a71d55c0a00
Brian Arthur <b-arthur@live.ie>
2024-04-12 15:47:40 +0100

    Initial Commit

---
+++ CacheExercise/GenericCache.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CacheExercise
+{
+    public class GenericCache
+    {
+        private Dictionary<int, object> _objectCache;
+        private Queue<int> _ids;
+        private int _cacheSize;
+        public GenericCache(int cacheSize) { 
+            _ids = new Queue<int>(cacheSize);
+            _objectCache = new Dictionary<int, object>(cacheSize);
+            _cacheSize = cacheSize;
+        }
+
+        public void Add(int id, object value)
+        {
+            if(_objectCache.Count == _cacheSize) {
+                //TODO Handle removal of least recently used. Pop it out and continue
+            }
+            //TODO add new value with id as key
+        }
+
+    }
+}
