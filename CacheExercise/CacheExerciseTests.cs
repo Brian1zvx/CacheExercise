e79002be5dd88ff7925e974e580f220f7aac9b28
Brian Arthur <b-arthur@live.ie>
2024-04-15 14:07:05 +0100

    Tests Added

---
+++ CacheExercise/CacheExerciseTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheExercise
+{
+    
+    [TestFixture]
+    public class CacheExerciseTests
+    {            
+        [Test]
+        public void Add_AddsObject_When_Called()
+        {
+            var testCache = new GenericCache(5);
+            var removedValue = testCache.Add(1, "ObjectOne");
+            
+            var value = testCache.Get(1);
+            
+            Assert.That(removedValue, Is.Null);
+            Assert.That(value, Is.EqualTo("ObjectOne"));
+
+        }
+
+        [Test]
+        public void Add_RemovesOldest_When_CacheIsFull()
+        {
+            var testCache = new GenericCache(1);
+            var firstVal = "ObjectOne";
+            var removedValue = testCache.Add(1, firstVal);
+            
+            Assert.That(removedValue, Is.Null);
+            var secondVal = "ObjectTwo";
+            removedValue = testCache.Add(2, secondVal);
+
+            Assert.That(removedValue, Is.Not.Null);
+            Assert.That(removedValue, Is.EqualTo(firstVal));
+            Assert.Throws<KeyNotFoundException>(() => testCache.Get(1));
+     
+            var value = testCache.Get(2);
+            Assert.That(value, Is.EqualTo(secondVal)); 
+
+        }
+
+        [Test]
+        public void Remove_RemovesValueAtId_When_Called()
+        {
+            var testCache = new GenericCache(3);
+            var firstVal = "ObjectOne";
+            var removedValue = testCache.Add(1, firstVal);
+
+            var secondVal = "ObjectTwo";
+            removedValue = testCache.Add(2, secondVal);
+
+            var thirdVal = "ObjectThree";
+            removedValue = testCache.Add(3, thirdVal);
+
+            testCache.Remove(2);
+            Assert.Throws<KeyNotFoundException>(() => testCache.Get(2));
+            Assert.That(testCache.Get(1), Is.EqualTo(firstVal));
+            Assert.That(testCache.Get(3), Is.EqualTo(thirdVal));
+
+        }
+
+        [Test]
+        public void Clear_MakesTheCacheEmpty()
+        {
+            var testCache = new GenericCache(3);
+            var firstVal = "ObjectOne";
+            var removedValue = testCache.Add(1, firstVal);
+
+            var secondVal = "ObjectTwo";
+            removedValue = testCache.Add(2, secondVal);
+
+            testCache.Clear();
+
+            Assert.Throws<KeyNotFoundException>(() => testCache.Get(1));
+            Assert.Throws<KeyNotFoundException>(() => testCache.Get(2));
+        }
+
+    }
+}
--- CacheExercise/obj/Debug/net8.0/CacheExercise.AssemblyInfo.cs
+++ CacheExercise/obj/Debug/net8.0/CacheExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CacheExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6729af97af1880aca9c63da0b7ea729874101ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301bea4a1a07380764cb6e2a97330d43e41436f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CacheExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CacheExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
